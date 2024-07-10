6bb45472c37c0b7904bce701e391a2d0a88cd93e
jenn-nam <151115629+jenn-nam@users.noreply.github.com>
2024-07-10 21:36:07 +0000

    assignment 6 modifications

--- Assignments/Assignment_06/TrainLine.java
+++ Assignments/Assignment_06/TrainLine.java
@@ -64,6 +64,11 @@ public class TrainLine {
             // The trainline has an existing head station. Therefore,
             // it also has a known last station (this.tail).
             this.tail.setNext(station); // add new station after tail station
+           /**
+            * updated line 
+            */
+            //link new station prev pointer to current tail of train line 
+            station.setPrev(this.tail); 
             this.tail = station; // Designate newly added station as tail station
         }
         // Update station counter
